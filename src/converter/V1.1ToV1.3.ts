ed6ca48f05ec03652a3e096d6e08229a78b6b360
NikBel3476 <nikita.belkin3476@gmail.com>
2023-07-05 00:09:50 +0400

    feature: create init version

--- src/converter/V1.1ToV1.3.ts
+++ src/converter/V1.1ToV1.3.ts
@@ -7,45 +7,40 @@ import {
 } from '../domain/v1.3/BuildingEntities';
 
 export const V1_1ToV1_3 = (bimJsonV1_1: BuildingJsonV1_1) => {
-	try {
-		const building: BuildingJsonV1_3 = {
-			fileData: {
-				dateOfCreation: new Date().toISOString(),
-				formatVersion: Number(
-					new Date().toISOString().substring(0, 10).split('-').join('')
-				),
-				usedProgram: {
-					name: '',
-					version: ''
-				},
-				version: '1.3'
+	const building: BuildingJsonV1_3 = {
+		fileData: {
+			dateOfCreation: new Date().toISOString(),
+			formatVersion: Number(
+				new Date().toISOString().substring(0, 10).split('-').join('')
+			),
+			usedProgram: {
+				name: '',
+				version: ''
 			},
-			buildingData: {
-				name: bimJsonV1_1.NameBuilding,
-				address: {
-					city: bimJsonV1_1.Address.City,
-					streetAddress: bimJsonV1_1.Address.StreetAddress,
-					additionalInfo: bimJsonV1_1.Address.AddInfo
-				},
-				levels: bimJsonV1_1.Level.map<Level>(level => ({
-					name: level.NameLevel,
-					zLevel: level.ZLevel,
-					elements: level.BuildElement.map<BuildingElement>(buildElement => ({
-						id: buildElement.Id,
-						name: buildElement.Name,
-						sizeZ: buildElement.SizeZ,
-						sign: buildElement.Sign as Sign,
-						outputs: buildElement.Output,
-						points: buildElement.XY[0].points,
-						type: 0
-					}))
+			version: '1.3'
+		},
+		buildingData: {
+			name: bimJsonV1_1.NameBuilding,
+			address: {
+				city: bimJsonV1_1.Address.City,
+				streetAddress: bimJsonV1_1.Address.StreetAddress,
+				additionalInfo: bimJsonV1_1.Address.AddInfo
+			},
+			levels: bimJsonV1_1.Level.map<Level>(level => ({
+				name: level.NameLevel,
+				zLevel: level.ZLevel,
+				elements: level.BuildElement.map<BuildingElement>(buildElement => ({
+					id: buildElement.Id,
+					name: buildElement.Name,
+					sizeZ: buildElement.SizeZ,
+					sign: buildElement.Sign as Sign,
+					outputs: buildElement.Output,
+					points: buildElement.XY[0].points,
+					type: 0
 				}))
-			}
-		};
+			}))
+		}
+	};
 
-		return building;
-	} catch (e) {
-		console.log('Something went wrong');
-		console.log(e);
-	}
+	return building;
 };
---
+++ src/main.ts
@@ -0,0 +1,97 @@
+import './style.css';
+import { ConversionMappingKeys, SUPPORTED_CONVERSIONS_MAPPING } from './consts.ts';
+
+let selectedConversion: ConversionMappingKeys;
+
+const versionSelect = document.getElementById('version_select') as HTMLSelectElement;
+const fileInput = document.getElementById('file_input') as HTMLInputElement;
+const convertButton = document.getElementById('convert_button') as HTMLButtonElement;
+const downloadSection = document.getElementById('download_section') as HTMLElement;
+const errorsSection = document.getElementById('errors_section') as HTMLElement;
+
+// options setup
+versionSelect.addEventListener('change', onVersionSelectChange);
+convertButton.addEventListener('click', onConvertButtonClick);
+
+const supportedConversions: Array<ConversionMappingKeys> = Object.keys(
+	SUPPORTED_CONVERSIONS_MAPPING
+) as Array<ConversionMappingKeys>;
+const options = supportedConversions.map(supportedConversion => {
+	const option = document.createElement('option');
+	option.value = supportedConversion;
+	option.innerText = supportedConversion;
+	return option;
+});
+versionSelect.append(...options);
+selectedConversion = supportedConversions[0];
+
+function onVersionSelectChange(event: Event) {
+	const target = event.currentTarget as HTMLSelectElement | null;
+	if (target) {
+		selectedConversion = target.value as ConversionMappingKeys;
+	}
+}
+
+async function onConvertButtonClick() {
+	await convertBimJson();
+}
+
+function addFilesToDownload(files: File[]) {
+	const downloadLinks = files.map(file => {
+		const link = document.createElement('a');
+		link.innerText = file.name;
+		link.download = file.name;
+		link.href = window.URL.createObjectURL(file);
+		link.className = 'converted_file_link';
+		return link;
+	});
+
+	downloadSection.innerHTML = '';
+	const title = document.createElement('h2');
+
+	title.innerText = 'Преобразованные файлы:';
+	downloadSection.appendChild(title);
+	downloadSection.append(...downloadLinks);
+}
+
+function showErrors(errorMessages: string[]) {
+	const errorElements = errorMessages.map(errorMessage => {
+		const errorElement = document.createElement('p');
+		errorElement.innerText = errorMessage;
+		errorElement.className = 'error_message';
+		return errorElement;
+	});
+
+	const title = document.createElement('h2');
+	title.innerText = 'Ошибки:';
+
+	errorsSection.innerHTML = '';
+	errorsSection.appendChild(title);
+	errorsSection.append(...errorElements);
+}
+
+async function convertBimJson() {
+	if (fileInput.files) {
+		const conversionErrorMessages = [];
+		const convertedFiles: File[] = [];
+		const convertingFunction = SUPPORTED_CONVERSIONS_MAPPING[selectedConversion];
+		for (const file of fileInput.files) {
+			try {
+				const json = JSON.parse(await file.text());
+				const convertedBim = convertingFunction(json);
+				convertedFiles.push(
+					new File([JSON.stringify(convertedBim)], file.name, {
+						type: 'application/json'
+					})
+				);
+			} catch (e) {
+				conversionErrorMessages.push(`${file.name} - не удалось преобразовать файл`);
+			}
+		}
+
+		addFilesToDownload(convertedFiles);
+		if (conversionErrorMessages.length > 0) {
+			showErrors(conversionErrorMessages);
+		}
+	}
+}
